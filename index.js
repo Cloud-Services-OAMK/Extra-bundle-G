1dd3c541ab29ee8d6719a672921d45c823622756
Kacper Jan Kolodziej <89990168+WellyBelly@users.noreply.github.com>
2022-10-08 01:44:52 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const listener = function (req,res) {
     res.statusCode = 200;
     res.setHeader('Content-type', 'text/plain');
-    res.end('Hello world + new content');
+    res.end('Hello world + really new content');
 }
 
 const server = http.createServer(listener);
