ccaee2996cc7ae75dd9036f15ba2e4fe2fb42d9a
unknown <89990168+WellyBelly@users.noreply.github.com>
2022-10-08 01:01:27 +0300

    Created very simple 'hello world' app

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const listener = function (req,res) {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    res.end('Hello world');
+}
+
+const server = http.createServer(listener);
+
+server.listen(process.env.PORT || 3000);
